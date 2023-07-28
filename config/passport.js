e7615590a905f44dcda28c900b97749fa8750f56
layton-skidmore <layton.skidmore@gmail.com>
2023-07-27 19:58:06 -0500

    CSS Changes

--- config/passport.js
+++ config/passport.js
@@ -4,26 +4,18 @@ const User = require('../models/user');
 const SpotifyStrategy = require('passport-spotify').Strategy
 
 passport.use(new GoogleStrategy(
-  // Configuration object
+  
   {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_SECRET,
     callbackURL: process.env.GOOGLE_CALLBACK
   },
-  // The verify callback function...
-  // Marking a function as an async function allows us
-  // to consume promises using the await keyword
+  
   async function(accessToken, refreshToken, profile, cb) {
     console.log(profile);
-    
-    // When using async/await  we use a
-    // try/catch block to handle an error
     try {
-      // A user has logged in with OAuth...
       let user = await User.findOne({ googleId: profile.id });
-      // Existing user found, so provide it to passport
       if (user) return cb(null, user);
-      // We have a new user via OAuth!
       user = await User.create({
         name: profile.displayName,
         googleId: profile.id,
